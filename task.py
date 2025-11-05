6500f8ec2c718d0126342dcfae29207015149151
brya-cota <bryacota@gmail.com>
2025-11-05 11:30:07 -0700

    Tip Calculator

--- task.py
+++ task.py
@@ -1,30 +1,12 @@
-print("Welcome to the rollercoaster!")
-height = int(input("What is your height in cm? "))
+#Tip Calculater!
+print("Welcome to the tip calculator!")
+bill = float(input("What was the total bill? $"))
+tip = int(input("What percentage tip would you like to give? 10 12 15 "))
+people = int(input("How many people to split the bill? "))
+total_bill = float((bill * tip/100 + bill) / people)
+print("Each person should pay: $",(round(total_bill, 2)))
 
-if height >= 120:
-    print("You can ride the rollercoaster")
-    age = int(input("What is your age? "))
-    if age <= 12:
-        print("Please pay $5.")
-    elif age <= 18:
-        print("Please pay $7.")
-    else:
-        print("Please pay $12.")
-else:
-    print("Sorry you have to grow taller before you can ride.")
 
-#Add some if/elif/else statements to the BMI calculator so that it interprets the BMI values calculated.
-#If the bmi is under 18.5 (not including), print out "underweight"
-#If the bmi is between 18.5 (including) and 25 (not including), print out "normal weight"
-#If the bmi is 25 (including) or over, print out "overweight"
 
-weight = 85
-height = 1.85
-print(input("What is your bmi "))
-bmi = weight / (height ** 2)
-if bmi >= 25:
-    print("overweight")
-elif bmi >= 18.5:
-    print("normal weight")
-else:
-    print("underweight")
+
+
